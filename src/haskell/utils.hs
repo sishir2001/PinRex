9d9a015542bf7d59a528a0910d1435ba0f6f33c7
jaladi.sishir <jaladi.sishir@juspay.in>
2024-02-01 07:54:36 +0530

    Feat:Algo for haskell support

---
+++ src/haskell/utils.hs
@@ -0,0 +1,14 @@
+module Utils
+    (
+        intToList,
+        digitCharToInt
+    ) where
+
+import Data.Char (digitToInt,ord)
+
+intToList :: Int -> [Int]
+intToList = map digitToInt . show
+
+-- convert char to int
+digitCharToInt :: Char -> Int
+digitCharToInt c = ord c - ord '0'
